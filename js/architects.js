3777a997f7f76fc9389103030c0c90f434ffdfe1
Andrae Muys <andrae.muys@gmail.com>
2012-10-30 17:25:08 +1000

    Updated with d3 wordclouds

--- js/architects.js
+++ js/architects.js
@@ -28,3 +28,46 @@ var architects = [
 "K.Langer",
 "B.Wilson"
 ];
+
+var firms = [
+"Conrad & Gargett",
+"BVN",
+"Karl Langer",
+"Hayes & Scott",
+"Wilson Architects"
+];
+
+var clients = [
+"Arawa Tavern",
+"Bougainville Copper Ltd",
+"Carlton & United Breweries Queensland Ltd",
+"Christmas Residence",
+"Civic & Civic Pty Ltd",
+"Cyclone Housing",
+"Day Centres",
+"Department of the Co-ordinator General and the Queensland Agricultural College Council",
+"Glendale System Housing",
+"Hamlet Estate Housing",
+"Hervey Bay Retirement Village",
+"Home Units",
+"Inala Youth Club",
+"James Hardie Trading Ltd Office and Vehicle Parc",
+"Karana Community Centre",
+"Lamb Home",
+"Mermaid Town Houses",
+"MLC Offices",
+"Poultry Abattoir ",
+"Presbyterian Church of Australia",
+"Queensland Agricultural College",
+"Runaway Bay Hotel",
+"Runaway Town Pty Ltd",
+"Sanitarium Health Food Co.",
+"Silverton Centre",
+"Sports Union of University of Queensland",
+"State Government of Queensland",
+"Stirling Henry Pty Ltd Warehouse and Workshop",
+"Sub-Normal Children's Home",
+"Supreme Court Building",
+"Toombul Shopping Town",
+"Watkins Property and Administration Pty Ltd"
+];
---
+++ js/clients.js
@@ -0,0 +1,36 @@
+var clients = [
+Arawa Tavern
+Bougainville Copper Ltd
+building
+Carlton & United Breweries Queensland Ltd
+Christmas Residence
+Civic & Civic Pty Ltd
+contemporary firm name
+Cyclone Housing
+Day Centres
+Department of the Co-ordinator General and the Queensland Agricultural College Council
+Glendale System Housing
+Hamlet Estate Housing
+Hervey Bay Retirement Village
+Home Units
+Inala Youth Club
+James Hardie Trading Ltd Office and Vehicle Parc
+Karana Community Centre
+Lamb Home
+Mermaid Town Houses
+MLC Offices
+Poultry Abattoir 
+Presbyterian Church of Australia
+Queensland Agricultural College
+Runaway Bay Hotel
+Runaway Town Pty Ltd
+Sanitarium Health Food Co.
+Silverton Centre
+Sports Union of University of Queensland
+State Government of Queensland
+Stirling Henry Pty Ltd Warehouse and Workshop
+Sub-Normal Children's Home
+Supreme Court Building
+Toombul Shopping Town
+Watkins Property and Administration Pty Ltd
+];
